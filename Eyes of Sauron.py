42cff29c6af8faa20668dcdfbe1fe0495bc1a143
GemanEmpire711 <162955723+GemanEmpire711@users.noreply.github.com>
2024-04-05 18:30:30 +0800

    Update Eyes of Sauron.py

--- Eyes of Sauron.py
+++ Eyes of Sauron.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord import Interaction
 from discord.ui import Select, View
 import config
+import database as d
 
 BOT_TOKEN = config.TOKEN
 CHANNEL_ID = 1216630854957404203
@@ -72,9 +73,9 @@ async def hello(interaction: discord.Interaction):
 ])
 async def list(interaction: discord.Interaction, list: int):
     if list == 1:
-        await cruncher(interaction)
+        await d.cruncher(interaction.response)
     elif list == 2:
-        await dignity(interaction)
+        await d.dignity(interaction.response)
 
 
 @bot.tree.command(name="watchlist", description="The Sauron has its eyes on these indiviaduals who has fallen prey.")
@@ -86,65 +87,5 @@ async def watchlist(interaction: Interaction):
         url="https://discord.com/invite/edot",
         emoji="<X_illyria:1018658471236861952>"))
     await interaction.response.send_message("The Watchful Eyes", view=view)
-
-async def cruncher(interaction: discord.Interaction):
-    embed = discord.Embed(
-        title="Crunchy Cork's Details",
-        description="Here are the details of Crunchy Cork:",
-        color=discord.Color.blurple()
-    )
-    embed.set_thumbnail(url="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-D6A22B4D634F4B10E2939FF53CB2927A-Png/150/150/AvatarHeadshot/Png/noFilter")
-
-    embed.add_field(name="Username", value="[Crunchy_Cork](https://www.roblox.com/users/2313514417/profile)", inline=True)
-    embed.add_field(name="KPH", value="87.52", inline=True)
-    embed.add_field(name="Notes", value="This guy is so hawt, I am drooling looking at his handsome face.", inline=False)
-    embed.add_field(name="Affiliation", value="Eagles of Illyria", inline=True)
-    embed.add_field(name="Rank", value="155", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Kills", value="84,458", inline=True)
-    embed.add_field(name="Hours", value="941", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Normalised Stats", value="", inline=False)
-    embed.add_field(name="KPH", value="5.5", inline=True)
-    embed.add_field(name="Hours",value="7.85", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Levels",value="8.05", inline=True)
-    embed.add_field(name="Kills",value="7.9", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Data Acquired", value="11th September 2023", inline=False)
-    embed.set_image(url="https://cdn.discordapp.com/attachments/1216630854957404203/1222804174564233256/image.png?ex=66178c21&is=66051721&hm=9583937218b7018ae3de6cc81871640a3d1d194d67301058084fe75c25b5f5fe&")
-    embed.set_footer(text="Powered by the all powerful Eyes of Sauron")
-    
-    await interaction.response.send_message(embed=embed)
-
-async def dignity(interaction: discord.Interaction):
-    embed = discord.Embed(
-        title="Diggitty_Doggitty's Details",
-        description="Here are the details of Diggitty_Doggitty:",
-        color=discord.Color.blurple()
-    )
-    embed.set_thumbnail(url="https://tr.rbxcdn.com/30DAY-AvatarHeadshot-7915A349D9703F317AF187AF57494554-Png/150/150/AvatarHeadshot/Png/noFilter")
-
-    embed.add_field(name="Username", value="[Diggitty_Doggitty](https://www.roblox.com/users/28486290/profile)", inline=True)
-    embed.add_field(name="KPH", value="141", inline=True)
-    embed.add_field(name="Notes", value="hi yes hello 😨😨😳😭😳😓🥺😔😬😨💀😎😎😎", inline=False)
-    embed.add_field(name="Affiliation", value="Eagles of Illyria", inline=True)
-    embed.add_field(name="Rank", value="135", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Kills", value="85,846", inline=True)
-    embed.add_field(name="Hours", value="613", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Normalised Stats", value="", inline=False)
-    embed.add_field(name="KPH", value="8.15", inline=True)
-    embed.add_field(name="Hours",value="5.6", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Levels",value="7.3", inline=True)
-    embed.add_field(name="Kills",value="8", inline=True)
-    embed.add_field(name="\u200b", value="\u200b", inline=True)
-    embed.add_field(name="Data Acquired", value="11th July 2023", inline=False)
-    embed.set_image(url="https://cdn.discordapp.com/attachments/1216630854957404203/1225715844475129856/image.png?ex=662223d6&is=660faed6&hm=086c7bf8a93087d70c6c1af27423fc084e3f1e778f537d4a64a606f2c2da2edf&")
-    embed.set_footer(text="Powered by the all powerful Eyes of Sauron")
-    
-    await interaction.response.send_message(embed=embed)
 
 bot.run(BOT_TOKEN)
